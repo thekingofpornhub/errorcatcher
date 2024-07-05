b313b2980298bded071830b4d60048720f1f2025
thekingofpornhub <167095167+thekingofpornhub@users.noreply.github.com>
2024-07-05 10:30:54 +0800

    Update for_match.py

--- lowlock/for_match.py
+++ lowlock/for_match.py
@@ -1,13 +1,18 @@
 import re
-import config
+
 def for_match_maybeerror(log):
     # 定义正则表达式模板来匹配错误信息
-   
+    MAYBEERROR_PATTERNS = [
+    re.compile(r'assertion failed', re.IGNORECASE),  # 匹配 assertion failed
+    re.compile(r'compiler error', re.IGNORECASE),  # 匹配 compiler error
+    re.compile(r'(error \w{5}):([^:]+):', re.IGNORECASE),
+    re.compile(r'(error \w{4}):([^:]+):', re.IGNORECASE)
+]
     # 用于存储匹配成功的内容
     matched_content = set()  # 使用集合来存储匹配内容，确保唯一性
-
+    
     # 匹配并合并结果
-    for pattern in config.MAYBEERROR_PATTERNS:
+    for pattern in MAYBEERROR_PATTERNS:
         for match in pattern.finditer(log):
             matched_text = match.group(0)
             matched_content.add(matched_text)  # 添加匹配文本到集合中
@@ -19,9 +24,12 @@ def for_match_assertionerror(log):
     # 定义正则表达式模板来匹配错误信息
     # 用于存储匹配成功的内容
     matched_content = set()  # 使用集合来存储匹配内容，确保唯一性
-
+    ASSERTIONERROR_PATTERNS = [
+    re.compile(r'assertion failed', re.IGNORECASE),  # 匹配 assertion failed
+    re.compile(r'compiler error', re.IGNORECASE),  # 匹配 compiler error
+]
     # 匹配并合并结果
-    for pattern in config.ASSERTIONERROR_PATTERNS:
+    for pattern in ASSERTIONERROR_PATTERNS:
         for match in pattern.finditer(log):
             matched_text = match.group(0)
             matched_content.add(matched_text)  # 添加匹配文本到集合中
